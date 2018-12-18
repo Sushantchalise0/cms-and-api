2e1d7da9e46967f57a66695418dccd5e7472cfde
sushant <sushant.chalise0@gmail.com>
2018-12-18 12:33:23 +0545

    db maintain

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    //mongoDbUrl: 'mongodb://sushant:sushant12@ds063879.mlab.com:63879/walkmancms'
-    mongoDbUrl: 'mongodb://localhost:27017/cms'
+    mongoDbUrl: 'mongodb://sushant:sushant12@ds063879.mlab.com:63879/walkmancms'
+    //mongoDbUrl: 'mongodb://localhost:27017/cms'
 };
