f39204542d092aabc02a18a06a83a2e8b19bd22d
sandeepmaharjan55 <sandeepmaharjan55@gmail.com>
2019-02-10 20:46:26 +0545

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //mongoDbUrl: 'mongodb://sandeep55:sandeep11@ds161074.mlab.com:61074/walkmancms'
-   //mongoDbUrl: 'mongodb://sushant:sushant12@ds063879.mlab.com:63879/walkmancms'
-   mongoDbUrl: 'mongodb://127.0.0.1:27017/walkmancms'
+   mongoDbUrl: 'mongodb://sushant:sushant12@ds063879.mlab.com:63879/walkmancms'
+   //mongoDbUrl: 'mongodb://127.0.0.1:27017/walkmancms'
 };
